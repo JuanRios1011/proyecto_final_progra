c2743c5454aa6eb354cef90b3a7dd902aa6d06db
JuanRios1011 <juandavidrios1624@gmail.com>
2024-11-17 12:48:02 -0500

    producto

---
+++ src/models/Producto.java
@@ -0,0 +1,72 @@
+package models;
+import java.time.LocalDate;
+
+public class Producto{
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int cantidadEnStock;
+
+    public Producto(int id, String nombre, String descripcion, double precio, int cantidadEnStock){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidadEnStock = cantidadEnStock;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidadEnStock() {
+        return cantidadEnStock;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidadEnStock(int cantidadEnStock) {
+        this.cantidadEnStock = cantidadEnStock;
+    }
+
+    public void actualizarCantidadEnStock(int nuevaCantidad){
+        this.cantidadEnStock = nuevaCantidad;
+    }
+
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", cantidadEnStock=" + cantidadEnStock +
+                '}';
+    }
+}
